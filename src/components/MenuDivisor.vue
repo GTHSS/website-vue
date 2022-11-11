d5f8b0e75c00b693b9a3069f135a5db2db2a8fc9
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-11-10 22:55:08 -0300

    Create UI components.

---
+++ src/components/MenuDivisor.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+    text: string,
+    overrideCss: string
+}>();
+let cssClass = "separator";
+let cssCustomized = "font-size: 15px";
+if (toBoolean(props.overrideCss)) {
+    cssClass = '';
+} else {
+    cssCustomized = '';
+}
+</script>
+
+<script lang="ts">
+function toBoolean(str: string): Boolean {
+    return str == 'true' || str == 'True';
+}
+</script>
+
+<template>
+    <h1 :class='cssClass' :style="cssCustomized">
+        {{ text }}
+    </h1>
+</template>
+
+<style scoped>
+.separator {
+    font-size: 15px,
+}
+</style>
