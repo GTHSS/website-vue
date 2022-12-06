b4376cbdaa5ba4a8a0eff3bd753194145c12319e
TheAirBlow <theairblow@example.com>
2022-12-06 06:59:20 +0000

    bruh

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import CenteredImage from "./components/CenteredImage.vue";
 
     <header>
         <CenteredImage imageTitle="Sussy Technology&trade;" imageAlt="Among Us Character Twerking"
-            imageSrc="./images/amongus_buttdance.webp" sizeX='100' sizeY="100" />
+            imageSrc="./images/amongus_buttdance.webp" imageSubTitle="Very suspicious indeed" sizeX='100' sizeY="100" />
     </header>
 
     <main>
@@ -36,10 +36,6 @@ import CenteredImage from "./components/CenteredImage.vue";
             objectText="NextCloud Storage" objectSubText="Dropbox but better" anchorLink="https://cloud.sussy.tech"
             menuColor="blue" />
 
-        <MenuItem imageLink="./images/notepad_logo.webp" imageAlternativeName="overflow_logo"
-            objectText="Server Overflow" objectSubText="Search for servers" anchorLink="https://search.sussy.tech"
-            menuColor="blue" />
-
         <MenuItem imageLink="https://source.sussy.tech/assets/img/logo.svg" imageAlternativeName="gitea_logo"
             objectText="Gitea Source Control" objectSubText="GitHub but better" anchorLink="https://source.sussy.tech"
             menuColor="green" />
@@ -54,13 +50,9 @@ import CenteredImage from "./components/CenteredImage.vue";
         <!--#endregion Available Sub-Websites-->
         <MenuDivisor text="Our Current projects" overrideCss="true" />
 
-        <MenuItem imageLink="./images/trollface.webp" imageAlternativeName="internet_troll" objectText="Troll Launcher"
-            objectSubText="Launches games made by us" anchorLink="javascript:alert('Not ready for public usage yet!')"
+        <MenuItem imageLink="./images/notepad_logo.webp" imageAlternativeName="overflow_logo"
+            objectText="Server Overflow" objectSubText="Search for servers" anchorLink="https://search.sussy.tech"
             menuColor="blue" />
-
-        <MenuItem imageLink="./images/blocky.png" imageAlternativeName="minecraft_logo" objectText="Blocky"
-            objectSubText="A modded block game experience"
-            anchorLink="javascript:alert('Not ready for public usage yet!')" menuColor="green" />
     </main>
 
 </template>
