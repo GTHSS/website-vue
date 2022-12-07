0f51d75cf07cec1d24ccfee213701baaefafec42
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-12-07 19:43:02 -0300

    prettier.

--- src/App.vue
+++ src/App.vue
@@ -6,56 +6,111 @@ import CenteredImage from "./components/CenteredImage.vue";
 </script>
 
 <template>
+  <header>
+    <CenteredImage
+      imageTitle="Sussy Technology&trade;"
+      imageAlt="Among Us Character Twerking"
+      imageSrc="./images/amongus_buttdance.webp"
+      imageSubTitle="Very suspicious indeed"
+      sizeX="100"
+      sizeY="100"
+    />
+  </header>
 
-    <header>
-        <CenteredImage imageTitle="Sussy Technology&trade;" imageAlt="Among Us Character Twerking"
-            imageSrc="./images/amongus_buttdance.webp" imageSubTitle="Very suspicious indeed" sizeX='100' sizeY="100" />
-    </header>
+  <main>
+    <MenuDivisor text="Our team" overrideCss="false" />
+    <PageAuthor
+      imageLink="./images/ivan.webp"
+      imageAlternativeName="Ivans Profile Picture"
+      objectText="not Ivan#4238"
+      objectSubText="He forgor everything"
+      anchorLink="https://discord.com/users/843818345291972629"
+      menuColor="blue"
+    />
 
-    <main>
-        <MenuDivisor text="Our team" overrideCss="false" />
-        <PageAuthor imageLink="./images/ivan.webp" imageAlternativeName="Ivans Profile Picture"
-            objectText="not Ivan#4238" objectSubText="He forgor everything"
-            anchorLink="https://discord.com/users/843818345291972629" menuColor="blue" />
+    <PageAuthor
+      imageLink="./images/airblow.webp"
+      imageAlternativeName="TheAirBlow PFP"
+      objectText="TheAirBlow#5375"
+      objectSubText="Some random guy"
+      anchorLink="https://discord.com/users/1011353533817290853"
+      menuColor="blue"
+    />
 
-        <PageAuthor imageLink="./images/airblow.webp" imageAlternativeName="TheAirBlow PFP"
-            objectText="TheAirBlow#5375" objectSubText="Some random guy"
-            anchorLink="https://discord.com/users/1011353533817290853" menuColor="blue" />
+    <PageAuthor
+      imageLink="./images/walter.jpg"
+      imageAlternativeName="Masqrades Profile Picture"
+      objectText="MasqradeOP⁴#6158"
+      objectSubText="Typical Rust enjoyer"
+      anchorLink="https://discord.com/users/1010290469235871785"
+      menuColor="blue"
+    />
 
-        <PageAuthor imageLink="./images/walter.jpg" imageAlternativeName="Masqrades Profile Picture"
-            objectText="MasqradeOP⁴#6158" objectSubText="Typical Rust enjoyer"
-            anchorLink="https://discord.com/users/1010290469235871785" menuColor="blue" />
+    <MenuDivisor text="Our projects" overrideCss="true" />
 
-        <MenuDivisor text="Our projects" overrideCss="true" />
+    <MenuItem
+      imageLink="./images/ball.png"
+      imageAlternativeName="overflow_logo"
+      objectText="ServerOverflow"
+      objectSubText="Spying on servers since 1998"
+      anchorLink="https://search.sussy.tech"
+      menuColor="brown"
+    />
 
-        <MenuItem imageLink="./images/ball.png" imageAlternativeName="overflow_logo"
-            objectText="ServerOverflow" objectSubText="Spying on servers since 1998" 
-            anchorLink="https://search.sussy.tech" menuColor="brown" />
+    <MenuDivisor text="Other stuff" overrideCss="true" />
 
-        <MenuDivisor text="Other stuff" overrideCss="true" />
+    <MenuItem
+      imageLink=" ./images/pterodactyl_logo.png"
+      imageAlternativeName="pterodactyl_logo"
+      objectText="Pterodactyl Control Panel"
+      objectSubText="Game servers done easy"
+      anchorLink="https://control.sussy.tech"
+      menuColor="brown"
+    />
 
-        <MenuItem imageLink=" ./images/pterodactyl_logo.png" imageAlternativeName="pterodactyl_logo"
-            objectText="Pterodactyl Control Panel" objectSubText="Game servers done easy"
-            anchorLink="https://control.sussy.tech" menuColor="brown" />
+    <MenuItem
+      imageLink="./images/ubuntu_logo.webp"
+      imageAlternativeName="ubuntu_logo"
+      objectText="Cockpit Management Panel"
+      objectSubText="Just because it looks cool"
+      anchorLink="https://manage.sussy.tech"
+      menuColor="orange"
+    />
 
-        <MenuItem imageLink="./images/ubuntu_logo.webp" imageAlternativeName="ubuntu_logo"
-            objectText="Cockpit Management Panel" objectSubText="Just because it looks cool"
-            anchorLink="https://manage.sussy.tech" menuColor="orange" />
+    <MenuItem
+      imageLink="./images/nextcloud_logo.webp"
+      imageAlternativeName="nextcloud_logo"
+      objectText="NextCloud Storage"
+      objectSubText="Dropbox but better"
+      anchorLink="https://cloud.sussy.tech"
+      menuColor="blue"
+    />
 
-        <MenuItem imageLink="./images/nextcloud_logo.webp" imageAlternativeName="nextcloud_logo"
-            objectText="NextCloud Storage" objectSubText="Dropbox but better" anchorLink="https://cloud.sussy.tech"
-            menuColor="blue" />
+    <MenuItem
+      imageLink="https://source.sussy.tech/assets/img/logo.svg"
+      imageAlternativeName="gitea_logo"
+      objectText="Gitea Source Control"
+      objectSubText="GitHub but better"
+      anchorLink="https://source.sussy.tech"
+      menuColor="green"
+    />
 
-        <MenuItem imageLink="https://source.sussy.tech/assets/img/logo.svg" imageAlternativeName="gitea_logo"
-            objectText="Gitea Source Control" objectSubText="GitHub but better" anchorLink="https://source.sussy.tech"
-            menuColor="green" />
+    <MenuItem
+      imageLink="./images/virgin_logo.webp"
+      imageAlternativeName="virgin_logo"
+      objectText="Ivan's router"
+      objectSubText="Virgin Media Hub 3.0"
+      anchorLink="https://router.sussy.tech"
+      menuColor="red"
+    />
 
-        <MenuItem imageLink="./images/virgin_logo.webp" imageAlternativeName="virgin_logo" objectText="Ivan's router"
-            objectSubText="Virgin Media Hub 3.0" anchorLink="https://router.sussy.tech" menuColor="red" />
-
-        <MenuItem imageLink="./images/blob_dance.gif" imageAlternativeName="emoji_cat_dance_gif"
-            objectText="Time to party" objectSubText="Wah? IDK either, just click it." anchorLink="./party.html"
-            menuColor="rainbow" />
-    </main>
-
+    <MenuItem
+      imageLink="./images/blob_dance.gif"
+      imageAlternativeName="emoji_cat_dance_gif"
+      objectText="Time to party"
+      objectSubText="Wah? IDK either, just click it."
+      anchorLink="./party.html"
+      menuColor="rainbow"
+    />
+  </main>
 </template>
